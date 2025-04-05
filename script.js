1ccf6ee3288f72d786535a6f8d3eb692b33e7cfa
Jackie Weckie <154088788+JackieWeckie@users.noreply.github.com>
2025-04-05 18:35:36 +0300

    hw4apr2025 [2]

--- script.js
+++ script.js
@@ -1,89 +1,73 @@
-
 //1
 
-let months = ['January', 'February', 'March', 'April', 'May',
-        'Jun', 'July', 'August', 'September', 'October', 'November', 'December'];
+const nums = [];
 
-reversed_months = months.reverse;
-console.log('Reversed month array: ', reversed_months);
 
-last_deleted_month = months.pop();
-console.log('Without the last month: ', last_deleted_month);
+let input = prompt('Введите любое число кроме 0. Число 0 завершит процесс выполнения.');
+const num = parseFloat(input);
 
-console.log('The last array element is ', months.push('December'));
+if (isNaN(num) || num == 0) {
+        alert('Вы ввели 0 или не число. Процесс завершён.');
+} else {
+        nums.push(num);
+}
+
+if (nums.length === 0) {
+        console.log('Введённых чисел нет');
+}
+
+const max = Math.max(...nums);
+const min = Math.min(...nums);
+const sum = nums.reduce((acc, val) => acc + val, 0);
+const average = sum / nums.length;
+
+console.log(`Максимальное значение: ${max}`);
+console.log(`Минимальное значение: ${min}`);
+console.log(`Сумма: ${sum}`);
+console.log(`Среднее арифметическое: ${average}`);
+
 
 //2
 
-const numbers = [];
-for (let i = 0; i < 10; i++) {
-        const number = prompt(`Введите случайные 10 чисел ${i + 1}`);
-        numbers.push(parseInt(number, 10));
+
+const x = prompt('Введите значение для X');
+const array = [];
+
+for (let i = 0; i < 5; i++) {
+        const element = prompt(`Введите элемент массива ${i + 1}:`);
+        array.push(element);
 }
 
-const evenNumbers = numbers.filter(number => number % 2 === 0);
-console.log('Чётные числа: ', numbers);
+const index = array.indexOf(x);
+
+if (index === -1) {
+        console.log('Значение X не найдено в массиве.');
+}
+
+const leftElement = index === 0 ? array[array.length - 1] : array[index - 1];
+const rightElement = index === array.length - 1 ? array[0] : array[index + 1];
+
+console.log(`Индекс X: ${index}`);
+console.log(`Левый соседний элемент: ${leftElement}`);
+console.log(`Правый соседний элемент: ${rightElement}`);
+
+
 
 //3
 
-const weekDays = [
-        'понедельник',
-        'вторник',
-        'среда',
-        'четверг',
-        'пятница',
-        'суббота',
-        'воскресенье'
-];
 
-const inputs = [];
+const evenNums = [];
+const oddNums = [];
 
-for (let i = 0; i < 7; i++) {
-        const input = prompt('Введите любой день недели');
-        inputs.push(input);
+for (let i = 0; i < 10; i++) {
+        const str = prompt(`Введите строку ${i + 1}`);
 }
 
-if (weekDays.length > 0) {
-        console.log('Дни недели, что вы ввели:', inputs);
+if (evenNums.length % 2 === 0) {
+        evenNums.push(str);
 } else {
-        console.log('Вы не ввели ни одного дня недели. Попробуйте ещё раз.');
-  
-const firstNum = parseFloat(prompt('Введите первое число'));
-const secondNum = parseFloat(prompt('Введите второе число'));
-
-if (isNaN(firstNum) || isNaN(secondNum)) {
-        alert('Вы не ввели число! Попробуйте ещё раз.')
-} else {
-        let operation = prompt('Выберите действие: сложение (+), вычитание (-), умножение (*) или деление (/)');
-
-        let result;
-
-        switch (operation) {
-                case '+': 
-                result = firstNum + secondNum;
-                break;
-
-                case '-':
-                result = firstNum - secondNum;
-                break;
-
-                case '*':
-                result = firstNum * secondNum;
-                break;
-
-                case '/':
-                if (secondNum === 0) {
-                        alert('Вы не можете делить на 0');
-                } else {
-                        result = firstNum / secondNum;
-                }
-                break;
-
-                default: 
-                alert('Некорректная операция. Попробуйте ещё раз.');
-                break;
-        }
-
-        if (result !== undefined) {
-                alert('Это будет ' + result);
-        }
+        oddNums.push(str);
 }
+
+console.log('Строки с чётными цифрами: ', evenNums);
+console.log('Строки с нечётными цифрами: ', oddNums);

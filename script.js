51d946a3c67fc51b8dc686310391aac1bea5fa2d
Jackie Weckie <154088788+JackieWeckie@users.noreply.github.com>
2025-04-14 19:28:47 +0300

    hw 11 april 2025 [2]

--- script.js
+++ script.js
@@ -1,45 +1,91 @@
-//1
-
-function isUnique(arr) {
-        const uniqueElements = new Set(arr);
-        return uniqueElements.size === arr.length;
-}
-
-console.log(isUnique([1, 2, 3, 4, 5, 6])); 
-console.log(isUnique([1, 2, 3, 2, 1])); 
-
-
-//2
-
-function mergeArrays(arr1, arr2) {
-        const mergedArray = [...new Set([...arr1, ...arr2])];
-        return mergedArray.sort((a, b) => a - b);
-}
-
-console.log(mergeArrays([1, 2, 3], [3, 4, 5]));
-
-//3
-
-function replaceNegatives(arr, replace) {
-        return arr.map(element => element < 0 ? replace : element);
-}
-
-console.log(replaceNegatives([-1, 2, -3, 4], 0));
-
-//4
-
-function isPrime(num) {
-        if (num <= 3) return true;
-        if (num % 2 === 0 || num % 3 === 0) return false;
-        for (let i = 5; i * i <= num; i += 6) {
-                if (num % i === 0 || num % (i + 2) === 0) return false;
+class CompanyWorkers {
+        constructor (worker) {
+                this.worker = worker;
+                this.employees = [];
+                this.positions = [];
         }
-        return true;
 }
 
-function getPrimes(arr) {
-        return arr.filter(isPrime);
+addEmployee(fullName, gender, age, position, salary) {
+        const employee = {
+                fullName,
+                gender,
+                age,
+                position,
+                salary
+        };
+        this.employees.push(employee);
+        if (!this.positions.includes(position)) {
+                this.positions.push(position);
+        }
+};
+
+removeEmployee(index) {
+        if (index >= 0 && index < this.employees.length) {
+                const removedEmployee = this.employees.splice(index, 1)[0];
+                if (removedEmployee.position === 'директор') {
+                        this.directorIndex = null;
+                }
+        } else {
+                console.log('Такого сотрудника не существует!');
+        }
 }
 
-console.log(isPrime(7));
-console.log(getPrimes([1, 2, 3, 4, 5, 6, 7]));
+updateEmployee(index) {
+        if (index >= 0 && index < this.employees.length) {
+                const employee = this.employees[index];
+                employee.fullName = prompt('Введите новое ФИО:', employee.fullName);
+                employee.gender = prompt('Введите новый пол:', employee.gender);
+                employee.age = prompt('Введите новый возраст:', employee.age);
+                employee.position = prompt('Введите новую должность:', employee.position);
+                employee.salary = prompt('Введите новую зарплату:', employee.salary);
+
+                if (!this.positions.includes(employee.position)) {
+                        this.positions.push(employee.position);
+                }
+
+                if (employee.position === 'директор') {
+                        this.directorIndex = index;
+                } else if (this.directorIndex === index) {
+                        this.directorIndex = null;
+                }
+        } else {
+                console.log('Некорректный индекс!');
+        }
+}
+
+emploeeList() {
+        this.employees.forEach((employee, index) => {
+                console.log(`${index + 1}. ${employee.fullName}, ${employee.gender}, ${employee.age}, ${employee.position}, ${employee.salary}`);
+        });
+}
+
+positionsList() {
+        this.positions.forEach(position => {
+                console.log(position);
+        });
+}
+
+getDirectorName() {
+        if (this.directorIndex !== null) {
+                return this.employees[this.directorIndex].fullName;
+        } else {
+                return 'Директор не назначен';
+        }
+}
+
+const myCompany = new Company('MyCompany');
+myCompany.addEmployee('Иванов Иван Иванович', 'Мужской', 45, 'Директор', 100000);
+myCompany.addEmployee('Петров Петр Петрович', 'Мужской', 30, 'Менеджер', 50000);
+myCompany.addEmployee('Сидорова Анна Сергеевна', 'Женский', 28, 'Бухгалтер', 45000);
+
+myCompany.listEmployees();
+myCompany.listPositions();
+console.log(`Директор: ${myCompany.getDirectorName()}`);
+
+myCompany.updateEmployee(1);
+myCompany.listEmployees();
+
+myCompany.removeEmployee(0);
+myCompany.listEmployees();
+console.log(`Директор: ${myCompany.getDirectorName()}`);
